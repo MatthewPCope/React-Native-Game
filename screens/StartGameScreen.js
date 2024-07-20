28b9844b8360f6916e8040b44c92607bd9cf0036
MatthewPCope <matthew.p.cope@gmail.com>
2024-07-20 10:18:29 -0700

    global colors

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { TextInput, View, StyleSheet, Alert} from  'react-native'
 import PrimaryButton from '../components/PrimaryButton'
+import Colors from '../constants/colors'
 
 function StartGameScreen({onPickNumber}) {
 
@@ -55,7 +56,7 @@ const styles = StyleSheet.create({
         alignItems:'center',
         marginTop:100,
         padding: 16,
-        backgroundColor: '#4e0329',
+        backgroundColor: Colors.primary800,
         marginHorizontal: 24,
         borderRadius: 8,
         elevation:4,
@@ -68,9 +69,9 @@ const styles = StyleSheet.create({
         height:50,
         width:50,
         fontSize: 32,
-        borderBottomColor:'#ddb52f',
+        borderBottomColor: Colors.accent500,
         borderBottomWidth: 2,
-        color: '#ddb52f',
+        color: Colors.accent500,
         marginVertical: 8,
         fontWeight: 'bold',
         textAlign: 'center'
